e2bdb145e6c96725c7553f6d24cc9f38dda06360
Jeroen Steenbeeke <jeroen.steenbeeke@topicus.nl>
2013-05-30 09:28:44 +0200

    Easy upload script for Linux

---
+++ upload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PORT=14128
+REMOTE_PATH=/var/lib/jetty/webapps
+URL=www.tysanclan.com
+ACCOUNT=root
+TARGET_NAME=dist.zip
+
+TARGET=`ls target/ProjectEwok*.war`
+scp -P $PORT $TARGET $ACCOUNT@$URL:$REMOTE_PATH/$TARGET_NAME
