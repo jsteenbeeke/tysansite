fb0f8d305015259e0c0609d74aaa2216c409c2cf
Jeroen Steenbeeke <jeroen.steenbeeke@topicus.nl>
2013-12-19 11:39:20 +0100

    Removed Accordion and Tabs components as Wicket components. Fixed banning of trial members

--- src/main/java/com/tysanclan/site/projectewok/components/BasicMemberPanel.java
+++ src/main/java/com/tysanclan/site/projectewok/components/BasicMemberPanel.java
@@ -43,7 +43,6 @@ import com.tysanclan.site.projectewok.components.RequiresAttentionLink.IRequires
 import com.tysanclan.site.projectewok.entities.AcceptanceVote;
 import com.tysanclan.site.projectewok.entities.AcceptanceVoteVerdict;
 import com.tysanclan.site.projectewok.entities.Bug;
-import com.tysanclan.site.projectewok.entities.Bug.ReportType;
 import com.tysanclan.site.projectewok.entities.ChancellorElection;
 import com.tysanclan.site.projectewok.entities.CompoundVote;
 import com.tysanclan.site.projectewok.entities.Event;
@@ -90,7 +89,6 @@ import com.tysanclan.site.projectewok.pages.member.CreateRealmPetitionPage;
 import com.tysanclan.site.projectewok.pages.member.EditAccountsPage;
 import com.tysanclan.site.projectewok.pages.member.EditUserGalleryPage;
 import com.tysanclan.site.projectewok.pages.member.EndorsementPage;
-import com.tysanclan.site.projectewok.pages.member.FeatureOverviewPage;
 import com.tysanclan.site.projectewok.pages.member.FeelingLuckyPage;
 import com.tysanclan.site.projectewok.pages.member.FinancePage;
 import com.tysanclan.site.projectewok.pages.member.JoinGroupPage;
@@ -151,30 +149,7 @@ public class BasicMemberPanel extends TysanOverviewPanel<Void> {
 		public AttentionType requiresAttention() {
 			if (getUser().isBugReportMaster()) {
 				for (Bug b : bugDAO.findAll()) {
-					if (b.getAssignedTo() == null
-							&& b.getReportType() != ReportType.FEATUREREQUEST)
-						return AttentionType.WARNING;
-				}
-
-			}
-			return null;
-		}
-
-		@Override
-		public Long getDismissableId() {
-			return null;
-		}
-	}
-
-	public class FeatureCondition implements IRequiresAttentionCondition {
-		private static final long serialVersionUID = 1L;
-
-		@Override
-		public AttentionType requiresAttention() {
-			if (getUser().isBugReportMaster()) {
-				for (Bug b : bugDAO.findAll()) {
-					if (b.getAssignedTo() == null
-							&& b.getReportType() == ReportType.FEATUREREQUEST)
+					if (b.getAssignedTo() == null)
 						return AttentionType.WARNING;
 				}
 
@@ -548,8 +523,12 @@ public class BasicMemberPanel extends TysanOverviewPanel<Void> {
 	private void addBugLink() {
 		add(createLink("bugs", BugOverviewPage.class, "Bugs Reports",
 				new BugCondition()));
+		add(createLink("newbug", ReportBugPage.class, "Report new bug",
+				new BugCondition()));
 		add(createLink("features", FeatureOverviewPage.class,
-				"FeatureRequests", new FeatureCondition()));
+				"FeatureRequests", NeverTrueCondition.get()));
+		add(createLink("newfeature", RequestFeaturePage.class,
+				"Request new feature", NeverTrueCondition.get()));
 	}
 
 	private void addGalleryLink(User user) {
